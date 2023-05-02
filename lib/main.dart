42055b4e039180fca681d178cfd74fda34570995
Aqib Saeed <aqibsaeedmahr@gmail.com>
2023-05-02 09:53:28 -0700

    mapping commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:foo_animation/detailpage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
 
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Mapping List'),
     );
   }
 }
@@ -34,26 +35,88 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var arrData = [
+    {
+      'name': 'Aqib Saeed',
+      'mob': '03116124245',
+      'unread': '2'
 
-@override
+    },
+    {
+      'name': 'Ahmed',
+      'mob': '03086040509',
+      'unread': '1',
+
+    },
+    {
+      'name': 'Taimoor',
+      'mob': '03036323121',
+      'unread': '5'
+
+    },
+    {
+      'name': 'Ali',
+      'mob': '03092124620',
+      'unread': '4'
+
+    },
+    {
+      'name': 'Zia',
+      'mob': '03105124259',
+      'unread': '8'
+
+    },
+    {
+      'name': 'Naveed',
+      'mob': '03212443333',
+      'unread': '2'
+
+    },
+    {
+      'name': 'Hamza',
+      'mob': '03112123222',
+      'unread' : '1'
+
+    },
+    {
+      'name': 'Kashif',
+      'mob': '03101212122',
+      'unread': '0'
+
+    }
+
+  ];
+
+  @override
   Widget build(BuildContext context) {
-
     return Scaffold(
         appBar: AppBar(
-
           title: Text(widget.title),
         ),
-        body: Center(
-          child: ClipRRect(
-            borderRadius: BorderRadius.only(topLeft: Radius.circular(45), bottomRight: Radius.circular(32)),
-            child: Image.asset(
-             'assets/images/logo.jpeg',
-                 width: 450,
-              height: 225,
-              fit: BoxFit.fill,
-              ),
+        body: Container(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListView(
+                children: arrData.map((value) =>
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListTile(
+                      leading: Icon(Icons.account_circle),
+                      title: Text(value['name'].toString()),
+                      subtitle: Text(value['mob'].toString()),
+                      trailing: CircleAvatar(
+                        radius: 12,
+                          backgroundColor: Colors.grey,
+                          child: Text(value['unread'].toString())),
+
+                    ),
+              )
+                ).toList()
             ),
-          )
-        );
+          ),
+
+        )
+    );
   }
+
 }
